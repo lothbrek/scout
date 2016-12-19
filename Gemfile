c835ef943de6d90b247cb1f3983114fbe1e1a5ac
MattSMcCullar <mattaym@gmail.com>
2016-12-18 19:05:09 -0600

    added file output to tweet IDs.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 gem 'sinatra'
 gem 'twitter'
+gem 'pry'
---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key 			 = "Input yours here."
+	config.consumer_secret 		 = "Input yours here."
+	config.access_token 			 = "Input yours here."
+	config.access_token_secret = "Input yours here."
+end
+
+#using my own twitter account for this, grabbing the latest 5 tweets
+#then printing results to the console.
+tweets = client.user_timeline('TalesOfMatteh', count: 5)
+tweets.each { |tweet| puts tweet.full_text }
+
+#write the resulting ids from the tweets array to results.txt
+results = File.open("results.txt", "w") { |f|
+	f.write(tweets)
+}
